ef09e1c182b0f1f4d48e34769d53d8a684872fa1
Rishabh <Rishabh-dev-12>
2025-06-13 22:24:43 +0530

    string again changes

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def root():
-    return "<h1>I LOVE YOU SONI</h1>"
+    return "<h1>I LOVE YOU MERA GOLU MOLU </h1>"
 
 
 app.run(port=4000, host="0.0.0.0")
